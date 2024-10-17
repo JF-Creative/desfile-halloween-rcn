d80ba141c225b909b599d5d7f8552c3bb6ece7cc
jcfranco <migranyami@gmail.com>
2024-10-17 10:33:52 -0500

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Counter from "./components/Counter";
+import SliderF6 from "./components/SliderF6";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src="kid.png" alt="kid" className="kid" />
+      <img src="logo1.png" alt="logo-desfile-disfraces" className="logo" />
+      <img src="baner1.png" alt="banner-desfile" className="banner" />
+      <div className="infoContainer">
+        <div className="counterContainer">
+          <Counter />
+        </div>
+        <h1 className="zelda">
+          <a
+            href="https://docs.google.com/forms/d/12wXmqzgv-07lQ_YWS21zFDfFjUwlHHYGb9C4f5EDNpM/viewform?edit_requested=true"
+            target="blank"
+          >
+            Haz clic aquí para inscribirte
+          </a>
+        </h1>
+      </div>
+      <div className="patreonContainer">
+        <img src="logos1.png" alt="patrocinadores" className="logos1" />
+        <div className="sliderContainer">
+          <SliderF6 />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Counter.jsx
@@ -0,0 +1,48 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import "./css/counter.css";
+
+export default function Countdown() {
+  const [timing, setTiming] = useState("");
+
+  useEffect(() => {
+    const intervalID = setInterval(() => {
+      const date = new Date();
+      const newYear = new Date("2024-10-31T15:30:00");
+      const timeRemain = newYear - date;
+
+      setTiming({
+        días: Math.floor(timeRemain / (1000 * 60 * 60 * 24)) % 30,
+        horas: Math.floor(timeRemain / (1000 * 60 * 60)) % 24,
+        minutos: Math.floor(timeRemain / (1000 * 60)) % 60,
+        segundos: Math.floor(timeRemain / 1000) % 60,
+      });
+    }, 1000);
+
+    return () => clearInterval(intervalID);
+  }, []);
+
+  return (
+    <div className="fullContainer">
+      <h2 className="title">Falta:</h2>
+      <div className="countdown">
+          <div className="timeBlock1">
+            <h2>{timing.días}</h2>
+            <h3>Días</h3>
+          </div>
+          <div className="timeBlock2">
+            <h2>{timing.horas}</h2>
+            <h3>Horas</h3>
+          </div>
+          <div className="timeBlock3">
+            <h2>{timing.minutos}</h2>
+            <h3>Min</h3>
+          </div>
+          <div className="timeBlock4">
+            <h2>{timing.segundos}</h2>
+            <h3>Seg </h3>
+          </div>
+        </div>
+    </div>
+  );
+}
