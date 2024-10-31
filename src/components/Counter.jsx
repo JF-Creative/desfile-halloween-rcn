e5b916ae78fd2a090ded2d11cb1e725cd2b9c155
jcfranco <migranyami@gmail.com>
2024-10-31 07:02:51 -0500

    counter updated

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -4,6 +4,10 @@ import "./css/counter.css";
 
 export default function Countdown() {
   const [timing, setTiming] = useState("");
+  const date = new Date();
+  const newYear = new Date("2024-10-31T15:30:00");
+  const today = new Date("2024-10-31T00:00:00");
+  const thanks = new Date("2024-10-31T00:00:00");
 
   useEffect(() => {
     const intervalID = setInterval(() => {
@@ -22,27 +26,69 @@ export default function Countdown() {
     return () => clearInterval(intervalID);
   }, []);
 
-  return (
-    <div className="fullContainer">
-      <h2 className="title">Falta:</h2>
-      <div className="countdown">
-          <div className="timeBlock1">
-            <h2>{timing.días}</h2>
-            <h3>Días</h3>
+  if (date >= newYear){
+    return(
+      <div className="fullContainer">
+        <h2>¡Es hoy!</h2>
+        <h3>Nos vemos en Multicentro</h3>
+      </div>
+    )
+  }
+  
+  if (date >= today){
+    return(
+      <div className="fullContainer">
+        <h2 className="title">Falta:</h2>
+        <div className="countdown">
+            <div className="timeBlock2">
+              <h2>{timing.horas}</h2>
+              <h3>Hrs</h3>
+            </div>
+            <div className="timeBlock3">
+              <h2>{timing.minutos}</h2>
+              <h3>Min</h3>
+            </div>
+            <div className="timeBlock4">
+              <h2>{timing.segundos}</h2>
+              <h3>Seg </h3>
+            </div>
           </div>
-          <div className="timeBlock2">
-            <h2>{timing.horas}</h2>
-            <h3>Hrs</h3>
+      </div>
+    )
+  }
+
+  if (date >= thanks){
+    return(
+      <div className="fullContainer">
+        <h2>¡Gracias Ibagué!</h2>
+        <h3>La pasamos genial con nuestros niños y niñas</h3>
+      </div>
+    )
+  }
+
+  else{
+    return (
+      <div className="fullContainer">
+        <h2 className="title">Falta:</h2>
+        <div className="countdown">
+            <div className="timeBlock1">
+              <h2>{timing.días}</h2>
+              <h3>Días</h3>
+            </div>
+            <div className="timeBlock2">
+              <h2>{timing.horas}</h2>
+              <h3>Hrs</h3>
+            </div>
+            <div className="timeBlock3">
+              <h2>{timing.minutos}</h2>
+              <h3>Min</h3>
+            </div>
+            <div className="timeBlock4">
+              <h2>{timing.segundos}</h2>
+              <h3>Seg </h3>
+            </div>
           </div>
-          <div className="timeBlock3">
-            <h2>{timing.minutos}</h2>
-            <h3>Min</h3>
-          </div>
-          <div className="timeBlock4">
-            <h2>{timing.segundos}</h2>
-            <h3>Seg </h3>
-          </div>
-        </div>
-    </div>
-  );
+      </div>
+    );
+  }
 }
